7b27125abc28c793cf15d3c7a6faa3298c4e9833
miles <719713496@qq.com>
2025-07-17 23:45:03 +0800

    add: me_comp

---
+++ src/components/Me.tsx
@@ -0,0 +1,13 @@
+import cn from 'classnames'
+
+export default function Me() {
+    return <div className={cn(
+        "Me w-[300px] h-[300px] z-[100] overflow-hidden shadow-[0px_5px_12px_2px_rgba(17,17,17,0.5)]", 
+        "border-[1px] border-solid border-[#ffffff26]",
+        'hover:border-white hover:cursor-pointer',
+        'fixed bottom-2 left-2',
+        'duration-200'
+        )}>
+ 
+    </div>
+}
--- src/pages/HomeComponent/HomeComponent.tsx
+++ src/pages/HomeComponent/HomeComponent.tsx
@@ -1,3 +1,8 @@
+import Me from '@/components/Me'
+
+
 export default function HomeComponent() {
-  return <div className="w-full h-full text-[50px] bg-black"></div>;
+  return <div className="w-full h-full text-[50px] bg-gradient-to-t from-[#243b55] to-[#141e30]">
+    <Me/>
+  </div>;
 }
